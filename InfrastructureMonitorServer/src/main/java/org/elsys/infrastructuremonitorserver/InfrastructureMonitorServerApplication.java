5d895c53fba9956ecfce6665e6e6465b02163934
Quant14 <igrgames05@gmail.com>
2023-04-11 12:56:01 +0300

    Server initial setup

---
+++ InfrastructureMonitorServer/src/main/java/org/elsys/infrastructuremonitorserver/InfrastructureMonitorServerApplication.java
@@ -0,0 +1,13 @@
+package org.elsys.infrastructuremonitorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastructureMonitorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrastructureMonitorServerApplication.class, args);
+	}
+
+}
---
+++ InfrastructureMonitorServer/src/test/java/org/elsys/infrastructuremonitorserver/InfrastructureMonitorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elsys.infrastructuremonitorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureMonitorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
