a93492df9934eb0ea9fec3cd3f782274ad91e35b
Walhalen <101360891+Walhalen@users.noreply.github.com>
2023-04-17 23:45:59 +0300

    Update server_app_get.py

--- server_app_get.py
+++ server_app_get.py
@@ -1,5 +1,38 @@
 import requests
+import os
+import time
 
-response = requests.get('http://localhost:8080/')
-print(response.status_code)
-print(response.text)
+
+
+
+url_list = ['http://25.24.74.142:8080/api/get',
+             'http://25.24.74.142:8080/api/get', 
+             'http://25.24.74.142:8080/api/get']
+# в този списък трябва да има сървъри един от които е първия main сървъра който като се развали има резервни коитпя
+index1 = 0
+index2 = 0
+
+while (index2 < 2):
+    try:
+        response = requests.get(url_list[index1])
+        if(response.ok is True):
+            index2 = 0
+            os.system('cls')
+            text1 = response.text
+            text1 = text1.replace("{", "")
+            text1 = text1.replace("}", "")
+            text1 = text1.replace("[", "")
+            text1 = text1.replace("]", "\n")
+            text1 = text1.replace(",", ", ")
+            text1 = text1.replace(":", " : ")
+            print(text1)
+            time.sleep(5)
+    except requests.RequestException as exception:
+         index1+=1
+         if(index1 is len(url_list)):
+            index2 += 1
+            index1 = 0
+    
+
+os.system('cls')        
+print("All server failed")
